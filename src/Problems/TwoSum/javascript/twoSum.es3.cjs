97115733bb9ed0defd3d8717beb098d410906e68
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-07-09 13:24:26 -0700

    Fix lint issue

--- src/Problems/TwoSum/javascript/twoSum.es3.cjs
+++ src/Problems/TwoSum/javascript/twoSum.es3.cjs
@@ -10,8 +10,8 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-
+var twoSum = function (nums, target) { // eslint-disable-line no-unused-vars
+  return 0;
 };
 // @lc code=end
 
