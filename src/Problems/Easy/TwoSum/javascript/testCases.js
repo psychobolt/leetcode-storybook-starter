ca61ae0d084d7f4b8440d64603a74f7483e21260
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-07-29 14:00:59 -0700

    refactor

--- src/Problems/Easy/TwoSum/javascript/testCases.js
+++ src/Problems/Easy/TwoSum/javascript/testCases.js
@@ -1,7 +1,5 @@
-import cloneDeep from 'lodash/fp/cloneDeep.js';
-
 import json from '../testCases.json';
 
 export const cases = json;
 
-export default (runSolution, { input }) => runSolution(cloneDeep(input));
+export default (runSolution, { input }) => runSolution(input);
